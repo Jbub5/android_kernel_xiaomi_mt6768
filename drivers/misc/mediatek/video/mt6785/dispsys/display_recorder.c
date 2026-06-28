be5dac3e0ae15b31644c8f4f5db56d476984f11d
Jbub5 <133902109+Jbub5@users.noreply.github.com>
2026-06-28 12:42:40 +0000

    Revert "drivers: mediatek: video: Fix function prototype warnings"
    This reverts commit d6b430fdb97f734d62e87efbb99a07425a0eed1f.

--- drivers/misc/mediatek/video/mt6785/dispsys/display_recorder.c
+++ drivers/misc/mediatek/video/mt6785/dispsys/display_recorder.c
@@ -1107,7 +1107,7 @@ void dprec_logger_dump_reset(void)
 	       sizeof(dprec_string_buffer_analysize));
 }
 
-char *dprec_logger_get_dump_addr(void)
+char *dprec_logger_get_dump_addr()
 {
 	return dprec_string_buffer_analysize;
 }
