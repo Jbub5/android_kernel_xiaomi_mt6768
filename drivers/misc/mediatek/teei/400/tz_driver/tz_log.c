3b1aa8ba78078584e52206c6473d11c65ffd8e30
Jbub5 <133902109+Jbub5@users.noreply.github.com>
2026-06-28 12:47:47 +0000

    Revert "teei: Disable TEEI log thread when !CONFIG_MICROTRUST_TZ_LOG"
    This reverts commit 15bf28614232ec0b764f5ff140b1d49aa2e43b13.

--- drivers/misc/mediatek/teei/400/tz_driver/tz_log.c
+++ drivers/misc/mediatek/teei/400/tz_driver/tz_log.c
@@ -38,8 +38,6 @@
 #define TEE_LOG_TYPE	0x10
 
 struct tz_log_state *g_tz_log_state;
-
-#ifdef CONFIG_MICROTRUST_TZ_LOG
 static struct completion teei_log_comp;
 
 int init_tlog_comp_fn(void)
@@ -54,7 +52,6 @@ void teei_notify_log_fn(void)
 	if (!completion_done(&teei_log_comp))
 		complete(&teei_log_comp);
 }
-#endif
 
 static int __tz_driver_read_logs(struct tz_log_state *s, char *buffer,
 				uint32_t get, unsigned int cnt)
@@ -212,14 +209,15 @@ static void tz_driver_dump_logs(struct tz_log_state *s)
 	s->get = get;
 }
 
-#ifdef CONFIG_MICROTRUST_TZ_LOG
 int teei_log_fn(void *work)
 {
 	int retVal = 0;
+#ifdef CONFIG_MICROTRUST_TZ_LOG
 	struct tz_log_state *s;
 	unsigned long flags;
 
 	s = g_tz_log_state;
+#endif
 
 	while (1) {
 		if (switch_input_index == switch_output_index) {
@@ -229,13 +227,15 @@ int teei_log_fn(void *work)
 				continue;
 		}
 
+#ifdef CONFIG_MICROTRUST_TZ_LOG
 		msleep(20);
 		tz_driver_dump_logs(s);
+#endif
+
 	}
 
 	return NOTIFY_OK;
 }
-#endif
 
 static int tz_log_panic_notify(struct notifier_block *nb,
 				   unsigned long action, void *data)
